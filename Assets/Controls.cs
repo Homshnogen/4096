e28acd64d5db6c532fd1178d69c85e585772f03f
Homshnogen <homshnogen@gmail.com>
2022-03-15 20:02:53 -0400

    moved files to github

---
+++ Assets/Controls.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            timer += 1f;
+            foreach(GridTile tile in GameObject.FindObjectsOfType<GridTile>())
+            {
+                if (tile.next[0] == null) tile.Carry(1);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            timer += 1f;
+            foreach (GridTile tile in GameObject.FindObjectsOfType<GridTile>())
+            {
+                if (tile.next[1] == null) tile.Carry(0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            timer += 1f;
+            foreach (GridTile tile in GameObject.FindObjectsOfType<GridTile>())
+            {
+                if (tile.next[2] == null) tile.Carry(3);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            timer += 1f;
+            foreach (GridTile tile in GameObject.FindObjectsOfType<GridTile>())
+            {
+                if (tile.next[3] == null) tile.Carry(2);
+            }
+        }
+    }
+}
---
+++ Assets/GridTile.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+public class GridTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GridTile[] next = { null, null, null, null };
+    public int value = 0;
+    private Material mat;
+    private Color color;
+    void Start()
+    {
+        value = 1;
+        mat = gameObject.GetComponent<Renderer>().material;
+        color = (value == 0) ? Color.white : Color.HSVToRGB((float)value / 20f, 1f, 1f);
+        mat.SetColor("_Color", color);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Carry(int dir)
+    {
+        if (next[dir] == null)
+        {
+            UpdateColor(1 ^ dir);
+        }
+        else if (value == 0)
+        {
+            next[dir].Carry(dir);
+        }
+        else if (next[dir].value == 0)
+        {
+            next[dir].value = value;
+            value = 0;
+            if (next[1 ^ dir] != null)
+            {
+                next[1 ^ dir].Carry(dir);
+            }
+            else
+            {
+                next[dir].Carry(dir);
+            }
+        }
+        else if (next[dir].value == value)
+        {
+            next[dir].value++;
+            value = 0;
+            if (next[1 ^ dir] != null)
+            {
+                next[1 ^ dir].Carry(dir);
+            }
+            else
+            {
+                next[dir].Carry(dir);
+            }
+        }
+        else
+        {
+            next[dir].Carry(dir);
+        }
+
+    }
+    public void MoveOnly(int dir)
+    {
+        if (next[dir] == null)
+        {
+        }
+        else if (value == 0)
+        {
+            next[dir].Carry(dir);
+        }
+        else if (next[dir].value == 0)
+        {
+            next[dir].value = value;
+            value = 0;
+            if (next[1 ^ dir] != null)
+            {
+                next[1 ^ dir].Carry(dir);
+            }
+            else
+            {
+                next[dir].Carry(dir);
+            }
+        }
+        else
+        {
+            next[dir].Carry(dir);
+        }
+
+    }
+
+    // do this later put a merge feature function
+
+
+    private void UpdateColor(int dir)
+    {
+        color = (value == 0) ? Color.white : Color.HSVToRGB((float) value / 20f, 1f, 1f);
+        mat.SetColor("_Color", color);
+        if (next[dir] == null) return;
+        next[dir].UpdateColor(dir);
+    }
+
+}
